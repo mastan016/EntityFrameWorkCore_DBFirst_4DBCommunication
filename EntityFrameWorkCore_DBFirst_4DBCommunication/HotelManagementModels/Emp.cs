eb5668070798e05a0e83fef07315c09b8abaed00
mastan <mastan.shamsu@gmail.com>
2026-05-05 07:47:31 +0530

    Db First Approach for hotel DB and Employee Table

---
+++ EntityFrameWorkCore_DBFirst_4DBCommunication/HotelManagementModels/Emp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EntityFrameWorkCore_DBFirst_4DBCommunication.HotelManagementModels;
+
+public partial class Emp
+{
+    public int? Eid { get; set; }
+
+    public string? Ename { get; set; }
+
+    public int? Salary { get; set; }
+
+    public int? Dno { get; set; }
+}
---
+++ EntityFrameWorkCore_DBFirst_4DBCommunication/HotelManagementModels/ProjectLevelErrorlog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EntityFrameWorkCore_DBFirst_4DBCommunication.HotelManagementModels;
+
+public partial class ProjectLevelErrorlog
+{
+    public int Id { get; set; }
+
+    public string? StatusCode { get; set; }
+
+    public string? ErrorMessage { get; set; }
+
+    public string? StackTraceError { get; set; }
+
+    public string? InnerExceptionError { get; set; }
+}
